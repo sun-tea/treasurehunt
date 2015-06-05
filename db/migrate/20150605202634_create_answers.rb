eda963021068a9044129d60663dda458f39fd9bc
mellenguyen <xthi.ngu@gmail.com>
2015-06-05 22:52:12 +0200

    MIMIMI

---
+++ db/migrate/20150605202634_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.string :title
+      t.string :value
+
+      t.timestamps null: false
+    end
+  end
+end
