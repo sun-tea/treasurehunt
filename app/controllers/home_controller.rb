b2bb1fa82b8d701f45c875f59ddad4ba1739e351
mellenguyen <xthi.ngu@gmail.com>
2015-06-06 01:09:42 +0200

    Controller side

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
 	def index
 		
 	end
+
+	def check
+		if params.has_key?(:facebook)
+			if Answer.find_by(title: "facebook").value == params[:facebook].strip
+    			redirect_to :back, notice: 'Bwavooooo'
+			end
+		end
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+logger = Logger.new(STDOUT)
+logger.debug('- - - - - - - - - - - - - - - - - - - - - - - - - -')
+
+answers = [
+        {:title => "facebook", :value => "06-06"},
+        {:title => "instagram", :value => "Rue de la Colonie"},
+        {:title => "lastfm", :value => "True Survivor"},
+        {:title => "twitter", :value => "Merguez"}
+]
+
+created_elems = 0
+
+answers.each do |answer|
+  if !(Answer.find_by(:title => answer[:title]))
+    Answer.create(answer)
+		logger.debug('Answer créée : ' + answer[:title])
+    created_elems += 1
+  end
+end
+
+logger.debug(' => ' + created_elems.to_s + ' answer(s) seeded')
+
